2d9ee8ab62c1d3bb13d95b5bcd1d0456e973ba88
Emmyginger <80985156+Emmyginger@users.noreply.github.com>
2021-05-18 22:03:05 +0100

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -1,28 +1,30 @@
 convertfahrTempToCelsius = (fahrTemp) => {
     if (fahrTemp ==null){
-      console.log(`please Enter a fahrenheit temperature for conversion`);
-    }
-    let regex = /[a-zA-Z]/
-    regexResult = regex.test(fahrTemp)
-    if (typeof fahrTemp === "boolean") {
-      console.log(`${fahrTemp}  is not a valid number but a/an boolean`);
-    }
-    else if (regexResult === true && typeof fahrTemp !== 'object' && typeof fahrTemp !== null) {
-      console.log(`'${fahrTemp}'  is not a valid number but a/an string`)
-    }
-    else if (Array.isArray(fahrTemp) === true) {
-      console.log(`[${fahrTemp}]  is not a valid number but a/an array`)
-    }
-    else if (typeof fahrTemp === 'object' && typeof fahrTemp !== null && !Array.isArray(fahrTemp)) {
-      console.log(`${JSON.stringify(fahrTemp)} is not a valid number but a/an object`)
-    }
+      console.log(`()  is not a valid number but a/an null`);
+    } 
     else {
-      let F = Number(fahrTemp)
-      let C = ((F - 32) * 100/180)
-      console.log(C.toFixed(4));
-    }
+        let regex = /[a-zA-Z]/
+        regexResult = regex.test(fahrTemp)
+        if (typeof fahrTemp === "boolean") {
+          console.log(`${fahrTemp}  is not a valid number but a/an boolean`);
+        }
+        else if (regexResult === true && typeof fahrTemp !== 'object' && typeof fahrTemp !== null) {
+          console.log(`'${fahrTemp}'  is not a valid number but a/an string`)
+        }
+        else if (Array.isArray(fahrTemp) === true) {
+          console.log(`[${fahrTemp}]  is not a valid number but a/an array`)
+        }
+        else if (typeof fahrTemp === 'object' && typeof fahrTemp !== null && !Array.isArray(fahrTemp)) {
+          console.log(`${JSON.stringify(fahrTemp)} is not a valid number but a/an object`)
+        }
+        else {
+          let F = Number(fahrTemp)
+          let C = ((F - 32) * 100/180)
+          console.log(C.toFixed(4));
+        }
+    }        
 }
-  convertfahrTempToCelsius("32")
+convertfahrTempToCelsius("32")
 
 
 checkYuGiOh = (input) => {
